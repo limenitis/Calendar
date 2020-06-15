2c057db69fa63209650217c2add1582c005ab76d
Nikita <ini.ivi@yandex.ru>
2020-06-15 22:17:00 +0300

    v 0.0.0.5 (change structure)

---
+++ options_m.py
@@ -0,0 +1,23 @@
+class Options():
+    
+    def new_event(self):
+        print('new_event work')
+        
+    def new_task(self):
+        print('new_task work')
+        
+    def change_layoyt_calendar(self):
+        print('change_layoyt_calendar work')
+
+    def change_layoyt_tasks(self):
+        print('change_layoyt_tasks work')
+
+    def delete_task(self):
+        print('delete_task work')
+
+    def update_task(self):
+        print('update_task work')
+        
+        
+def rgb(r,g,b,invis):
+    return (r/255,g/255,b/255,invis)
